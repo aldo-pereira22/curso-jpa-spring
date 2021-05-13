099a1c861528e3ee1ac16f55b54c7deb77fe4dae
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-05-13 19:59:23 -0300

    Criandooperação de instanciar os objetos em memoria

--- src/main/java/com/dev22/CursoJpaSpringApplication.java
+++ src/main/java/com/dev22/CursoJpaSpringApplication.java
@@ -1,13 +1,32 @@
 package com.dev22;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoJpaSpringApplication {
+import com.dev22.domain.Categoria;
+import com.dev22.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoJpaSpringApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJpaSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
  
 }
