92020dbb4e802857535cae658f9402a923f70254
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-05-12 23:31:00 -0300

    Projeto criado e configurado

---
+++ src/test/java/com/dev22/CursoJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
